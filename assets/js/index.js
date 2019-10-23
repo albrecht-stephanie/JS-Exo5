fe57c3101728669c30cf236ecb614f74b60b4174
AS <albrecht.stephanie@outlook.fr>
2019-10-23 23:27:17 +0200

    mise en page et verif saisie

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,25 @@
 
 function controle(){
+
   var firstNumber = document.getElementById("firstNumber").value;
   var secondNumber = document.getElementById("secondNumber").value;
+  if ((isNaN(firstNumber)) || (isNaN(secondNumber))){ //verification que les champs contiennent des nombresss
+    alert("J'ai dis des nombres !!!")
 
-  var a = Math.trunc(firstNumber);
-  var b = Math.trunc(secondNumber);
+  }
+  else {
+    if (Number.isInteger(firstNumber,secondNumber)){  //verification si nombres decimaux
+      alert("J'ai dis des nombres à virgule !!!")
+    }
+    else {
+      var a = Math.trunc(firstNumber);
+      var b = secondNumber;
 
-  var c = a * b;
+      var c = a * b;
 
-  alert("Résultat :"+c);
+      alert("Résultat :"+c);
+    }
+
+  }
+
 }
